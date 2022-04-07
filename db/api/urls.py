b33272b9be67b7aab441421e4d0ce6c7c9dce63a
dcatabian <catabiandb@gmail.com>
2022-04-07 16:48:53 -0600

    Endpoints started

--- db/api/urls.py
+++ db/api/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns = [
     #path('users/', views.UserList.as_view()),
     #path('users/<int:pk>', views.UserDetail.as_view()),
+    path('user?username=<String:Username>', views.getLogin.as_view()),
+    path('pr', views.pr.as_view()),
 ]
--- db/api/views.py
+++ db/api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import loginSerializer
+from .models import LoginInfo
 
 # Create your views here.
+
+class getLogin(APIView):
+    def get(self, request, format=None):
+        credentials = LoginInfo.objects.all()
+        serializer = loginSerializer(credentials, many = True)
+        return Response(serializer.data)
+
+class pr(APIView):
+    #def get
+
+    #def put
